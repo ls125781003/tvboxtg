8cc9122642e7f4522ab039e3b34c38ac639b8afa
xiaoliu <125781003@qq.com>
2024-03-11 06:27:37 +0000

    Pending changes exported from your codespace

--- 小胡/js/优酷.js
+++ 小胡/js/优酷.js
@@ -3,9 +3,8 @@ var rule = {
     host:'https://www.%79%6f%75%6b%75.com',
     homeUrl:'',
     searchUrl:'https://search.%79%6f%75%6b%75.com/api/search?pg=fypage&keyword=**',
-    searchable:2,
-    quickSearch:0,
-    filterable:1,
+    searchable:0,
+    changeable:0,
     multi:1,
     // 分类链接fypage参数支持1个()表达式
     url:'/category/data?optionRefresh=1&pageNo=fypage&params=fyfilter',
