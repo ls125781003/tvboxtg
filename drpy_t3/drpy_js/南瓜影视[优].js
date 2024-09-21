2d341a39e084e822447379cad2e8e5810589b6f2
xiaoliu <18409283+ls125781003@users.noreply.github.com>
2024-09-21 21:04:55 +0800

    Add files via upload

--- drpy_t3/drpy_js/南瓜影视[优].js
+++ drpy_t3/drpy_js/南瓜影视[优].js
@@ -1,9 +1,9 @@
 // 注入全局方法 (仅支持tvbox的js1以及c#版drpy的js0，暂不支持drpy官方py版的js0)
 // 注入全局方法 (仅支持tvbox的js1以及c#版drpy的js0，暂不支持drpy官方py版的js0)
 // 注入全局方法 (仅支持tvbox的js1以及c#版drpy的js0，暂不支持drpy官方py版的js0)
-globalThis.getHeaders = function (input) {
+globalThis.getHeaders= function(input){
     let t = new Date().getTime().toString();
-    let headers = {
+	let headers = {
         'version_name': '1.0.6',
         'version_code': '6',
         'package_name': 'com.app.nanguatv',
@@ -11,44 +11,47 @@ globalThis.getHeaders = function (input) {
         'imei': 'c431ea542cee9679',
         'timeMillis': t,
         'User-Agent': 'okhttp/4.6.0'
-    };
-    return headers
+	};
+	return headers
 }
-
+	
 var rule = {
-    title: '南瓜影视[优]',
-    host: 'http://ys.changmengyun.com',
-    homeUrl: '/api.php/provide/vod_rank?app=ylys&sort_type=month&imei=c431ea542cee9679&id=2&page=1',
-    url: '/api.php/provide/vod_list?app=ylys&id=fyclassfyfilter&page=fypage&imei=c431ea542cee9679',
-    detailUrl: '/api.php/provide/vod_detail?app=ylys&imei=c431ea542cee9679&id=fyid',
-    searchUrl: '/api.php/provide/search_result_more?app=ylys&video_name=**&pageSize=20&tid=0&imei=c431ea542cee9679&page=fypage',
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 1,
-    filter_url: '&area={{fl.area}}&year={{fl.year}}&type={{fl.class}}&total={{fl.total or "状态"}}&order={{fl.by or "新上线"}}',
-    filter: 'H4sIAAAAAAAAA+2X0XLSQBSG3yXXXARoKfRVnF5EhyuxzrTVGabDTCtSC21B6wxai9WOLcGxlKC1QjDty2Q38BYmJHvOWZwJdoxe5Y7vPz+bZXf/s2FTSSrL9zaVh/misqw8KGjr60pCWdUe5V10+iN2sufyU63wJD/1rboyq3Qm5Y4no6eUCGq1jm21nOqLoIwMjmaLVXXiABYOZ7fPyxV0IINDf8WGI+IAFg6+/ZJvNdGBDPOo6tJTkGGM3be2WSVjAIOjXOPP3hEHcGnF8/irqq3lNVxU1jLYvhm+qIEHpnpQH/e6ouaDqE3aR3zQw0FnJViymzo7tsR6+QA/40LnV1/Fb/ABxv/wGb8XAHzvzTlvXYjv+QBzbhh8eCPNS5JgjG8Gjh/AXcdgBwYz27A+U4Ba5ZpdlkXNB7o5xby2RjZneGWPrDmb43vEA1JqaiGoTD8SPY16muop1FNUT6KepLqKukr0ZA509yPRs6hnqb6E+hLVM6hnqL6I+iLoSXW6AJ+CGqCoq3Jdna3n5Hputp6V64B00+4Xcct4/ZCZjd+2zCl3+WGXt1viyAPDyTi3xr3n6ECGs9k07EHNMW/F8QR2J7OSUFLRts5jyzZ1tw2J+QDDfNxUNwx0IEfbbnYqriJqPty13cyNbhzBOIIRRDAdbQTP9MnRjpiMD3L4pOTRi9C9GvEi9IBcru51i5erB/8trpFOOr5R4ziHxnleWBf+wX3Jf36R7kuP5VOMDmQ5A8QBLCeIOIAjDbE96Lrzl/qPJP3JK3foq3oc6DjQ0QYaHrXxeEMrkCjXrvnW9pwo+x7xnPHtydgSJzcAOJ2X+87otThYPgTtJBNpP/GzT96ugeUQEQcwOL7/YGdN4gCOtFuEJV2a4ez0QrpH2B/x8V4fawGQM8JqH9n7UzwjAZP+Yo+a2F88iPtL3F/+/v2/9AsqovmovxQAAA==',
-    headers: {
-        "User-Agent": "okhttp/4.6.0"
-    },
-    timeout: 5000,
-    class_name: '电视剧&电影&动漫&综艺&海外精选', // /api.php/provide/home_nav
-    class_url: '2&1&4&3&46',
-    limit: 20,
-    play_parse: true,
-    lazy: `js:
+	title:'南瓜影视',
+	host:'http://ys.changmengyun.com',
+	homeUrl:'/api.php/provide/vod_rank?app=ylys&sort_type=month&imei=c431ea542cee9679&id=2&page=1',
+    url:'/api.php/provide/vod_list?app=ylys&id=fyclassfyfilter&page=fypage&imei=c431ea542cee9679',
+    detailUrl:'/api.php/provide/vod_detail?app=ylys&imei=c431ea542cee9679&id=fyid',
+    searchUrl:'/api.php/provide/search_result_more?app=ylys&video_name=**&pageSize=20&tid=0&imei=c431ea542cee9679&page=fypage',
+	searchable:2,
+	quickSearch:0,
+	filterable:1,
+	filter_url:'&area={{fl.area}}&year={{fl.year}}&type={{fl.class}}&total={{fl.total or "状态"}}&order={{fl.by or "新上线"}}',
+    filter:'H4sIAAAAAAAAA+2X0XLSQBSG3yXXXARoKfRVnF5EhyuxzrTVGabDTCtSC21B6wxai9WOLcGxlKC1QjDty2Q38BYmJHvOWZwJdoxe5Y7vPz+bZXf/s2FTSSrL9zaVh/misqw8KGjr60pCWdUe5V10+iN2sufyU63wJD/1rboyq3Qm5Y4no6eUCGq1jm21nOqLoIwMjmaLVXXiABYOZ7fPyxV0IINDf8WGI+IAFg6+/ZJvNdGBDPOo6tJTkGGM3be2WSVjAIOjXOPP3hEHcGnF8/irqq3lNVxU1jLYvhm+qIEHpnpQH/e6ouaDqE3aR3zQw0FnJViymzo7tsR6+QA/40LnV1/Fb/ABxv/wGb8XAHzvzTlvXYjv+QBzbhh8eCPNS5JgjG8Gjh/AXcdgBwYz27A+U4Ba5ZpdlkXNB7o5xby2RjZneGWPrDmb43vEA1JqaiGoTD8SPY16muop1FNUT6KepLqKukr0ZA509yPRs6hnqb6E+hLVM6hnqL6I+iLoSXW6AJ+CGqCoq3Jdna3n5Hputp6V64B00+4Xcct4/ZCZjd+2zCl3+WGXt1viyAPDyTi3xr3n6ECGs9k07EHNMW/F8QR2J7OSUFLRts5jyzZ1tw2J+QDDfNxUNwx0IEfbbnYqriJqPty13cyNbhzBOIIRRDAdbQTP9MnRjpiMD3L4pOTRi9C9GvEi9IBcru51i5erB/8trpFOOr5R4ziHxnleWBf+wX3Jf36R7kuP5VOMDmQ5A8QBLCeIOIAjDbE96Lrzl/qPJP3JK3foq3oc6DjQ0QYaHrXxeEMrkCjXrvnW9pwo+x7xnPHtydgSJzcAOJ2X+87otThYPgTtJBNpP/GzT96ugeUQEQcwOL7/YGdN4gCOtFuEJV2a4ez0QrpH2B/x8V4fawGQM8JqH9n7UzwjAZP+Yo+a2F88iPtL3F/+/v2/9AsqovmovxQAAA==',
+	headers:{
+		"User-Agent":"okhttp/4.6.0"
+	},
+	timeout:5000,
+	class_name:'电视剧&电影&动漫&综艺&海外精选', // /api.php/provide/home_nav
+	class_url:'2&1&4&3&46',
+	limit:20,
+	play_parse:true,
+tab_remove:['NG快播-1'],
+    
+lazy: $js.toString(() => {
         try {
             function getvideo(url) {
                 let jData = JSON.parse(request(url, {
                     headers: getHeaders(url)
                 }));
                 if (jData.code == 1) {
+                    
                     return jData.data.url
                 } else {
                     return 'http://43.154.104.152:1234/jhapi/cs.php?url=' + url.split('=')[1]
                 }
             }
             if (/,/.test(input)) {
-                let mjurl = input.split(',')[1]
+                let mjurl = input.split(',')[0]
                 let videoUrl = getvideo(mjurl);
                 input = {
                     jx: 0,
@@ -60,6 +63,7 @@ var rule = {
                 }
             } else {
                 let videoUrl = getvideo(input);
+                
                 if (/jhapi/.test(videoUrl)) {
                     videoUrl = getvideo(videoUrl);
                     input = {
@@ -71,6 +75,7 @@ var rule = {
                         })
                     }
                 } else {
+                    
                     input = {
                         jx: 0,
                         url: videoUrl,
@@ -81,8 +86,8 @@ var rule = {
         } catch (e) {
             log(e.toString())
         }
-	`,
-    推荐: `js:
+	}),
+	推荐:$js.toString(() => {
         var d = [];
         let html = request(input, {
             headers: getHeaders(input)
@@ -97,8 +102,8 @@ var rule = {
             })
         });
         setResult(d);
-    `,
-    一级: `js:
+    }),
+	一级:$js.toString(() => {
 		var d = [];
 		let html = request(input, {
 			headers: getHeaders(input)
@@ -113,8 +118,8 @@ var rule = {
 			})
 		});
 		setResult(d);
-	`,
-    二级: `js:
+	}),
+	二级:$js.toString(() => {
         var d = [];
         VOD = {
             vod_id: input.split('id=')[1]
@@ -145,8 +150,10 @@ var rule = {
                     if (!playMap.hasOwnProperty(source)) {
                         playMap[source] = []
                     }
-                    playMap[source].append(playurl['name'].strip() + '$' + play_url + urlencode(playurl['url']))
-                })
+                    playMap[source].append(playurl['name'].strip() + '$' +  
+                     //play_url + urlencode(playurl['url'][0])
+                     play_url + playurl['url'][0]
+                     )})
             });
             let playFrom = [];
             let playList = [];
@@ -162,8 +169,8 @@ var rule = {
         } catch (e) {
             log('获取二级详情页发生错误:' + e.message)
         }
-	`,
-    搜索: `js:
+	}),
+	搜索:$js.toString(() => {
         var d = [];
         let html = request(input, {
             headers: getHeaders(input)
@@ -179,5 +186,5 @@ var rule = {
             })
         });
         setResult(d);
-    `,
+    }),
 }
