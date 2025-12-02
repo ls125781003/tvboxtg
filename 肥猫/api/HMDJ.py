6e6267fbf9969ce6cb20d4e455575727efdd07f7
Liu <18409283+ls125781003@users.noreply.github.com>
2025-12-02 16:40:07 +0800

    更新线路
    整体线路：v1202
    潇洒线路：v1202

--- 肥猫/api/HMDJ.py
+++ 肥猫/api/HMDJ.py
@@ -1,1 +1,1 @@
-[session-e443f997] Route error: The Repository has been blocked. see: https://gitee.com/PizazzXS/another-d
+[session-c640e2b1] Route error: The Repository has been blocked. see: https://gitee.com/PizazzXS/another-d
--- 肥猫/api/LSYS.py
+++ 肥猫/api/LSYS.py
@@ -1,1 +1,1 @@
-[session-304a5bb0] Route error: The Repository has been blocked. see: https://gitee.com/PizazzXS/another-d
+[session-b5401fba] Route error: The Repository has been blocked. see: https://gitee.com/PizazzXS/another-d
